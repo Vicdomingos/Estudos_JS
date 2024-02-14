f03118d8e9e91c0847f7f11ac6c4a00d8c8e5146
Vitória Regina <vitoriaregina.ddd@gmail.com>
2024-02-14 15:21:06 -0300

    primeiros commits

---
+++ js/scripts.js
@@ -0,0 +1,53 @@
+// Selecionando os elementos
+
+// 1- getElementById
+// const cabecalho = document.getElementById('js-javascript');
+
+// console.log(cabecalho);
+
+
+// 2- querySelector
+// const teste = document.querySelector('.testando');
+
+// console.log(teste);
+
+// 3- querySelectorAll
+// const teste = document.querySelectorAll('.js-seletor'); 
+// usado só para classe
+
+// console.log(teste)
+
+// ForEach e Arrow function
+
+// 4-ForEach
+// const allteste = document.querySelectorAll('.js-seletor');
+
+// allteste.forEach(function(item) {
+    // console.log(item);
+// })
+
+
+// 5-parametros do ForEach
+// const allteste = document.querySelectorAll('.js-seletor');
+
+// allteste.forEach(function(itemAtual,index,array) {
+// console.log(itemAtual);  //Item atual do loop
+// console.log(index);  //O numero do index
+// console.log(array);  //Array completa
+// })
+
+// forEach usando o click
+// const allteste = document.querySelectorAll('.js-seletor');
+
+// allteste.forEach(function(itemAtual,index,array) {
+// itemAtual.addEventListener('click',function() {
+// console.log('Estou clicando no item' + index);
+    // })
+// })
+
+// 6- Arrow function
+// const allteste = document.querySelectorAll('.js-seletor');
+
+// allteste.forEach((item) => {
+    // console.log(item);
+// })
